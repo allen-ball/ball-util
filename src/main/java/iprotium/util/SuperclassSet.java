8be48d7f301f030366a4d408486fa67e882e6679
Allen D. Ball <ball@hcf.dev>
2008-10-27 21:56:11 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@25 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/SuperclassSet.java
@@ -0,0 +1,45 @@
+/*
+ * $Id: SuperclassSet.java,v 1.1 2008-10-27 21:56:11 ball Exp $
+ *
+ * Copyright 2008 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util;
+
+import java.util.TreeSet;
+
+/**
+ * TreeSet implementation that calculates the superclasses and
+ * super-interfaces of a Class.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public class SuperclassSet extends TreeSet<Class<?>> {
+    private static final long serialVersionUID = -8076508406076608539L;
+
+    public SuperclassSet(Class<?> type) {
+        super(ClassOrder.NAME);
+
+        addSuperclassesOf(type);
+    }
+
+    private void addSuperclassesOf(Class<?> type) {
+        if (type != null) {
+            if (! contains(type)) {
+                add(type);
+
+                addSuperclassesOf(type.getSuperclass());
+                addSuperclassesOf(type.getInterfaces());
+            }
+        }
+    }
+
+    private void addSuperclassesOf(Class<?>[] types) {
+        for (Class<?> type : types) {
+            addSuperclassesOf(type);
+        }
+    }
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
