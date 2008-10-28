016630cbbf92d7df5042150e0949706deb08ab14
Allen D. Ball <ball@hcf.dev>
2008-10-28 09:24:21 +0000

    Added <is-assignable-from/> target and Ant Task.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@31 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/ant/taskdefs/IsAssignableFromTask.java
@@ -0,0 +1,66 @@
+/*
+ * $Id: IsAssignableFromTask.java,v 1.1 2008-10-28 09:24:21 ball Exp $
+ *
+ * Copyright 2008 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util.ant.taskdefs;
+
+import iprotium.util.SuperclassSet;
+import org.apache.tools.ant.BuildException;
+
+import static iprotium.util.ClassOrder.INHERITANCE;
+
+/**
+ * Ant Task to display superclasses of a specified Class.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public class IsAssignableFromTask extends AbstractClasspathTask {
+    private String type = null;
+    private String subtype = null;
+
+    /**
+     * Sole constructor.
+     */
+    public IsAssignableFromTask() { super(); }
+
+    protected String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    protected String getSubtype() { return subtype; }
+    public void setSubtype(String subtype) { this.subtype = subtype; }
+
+    @Override
+    public void execute() throws BuildException {
+        if (getType() == null) {
+            throw new BuildException("`type' attribute must be specified");
+        }
+
+        if (getSubtype() == null) {
+            throw new BuildException("`subtype' attribute must be specified");
+        }
+
+        try {
+            Class<?> superclass = getClass(getType());
+            Class<?> subtype = getClass(getSubtype());
+
+            log(superclass.getName() + " is "
+                + (superclass.isAssignableFrom(subtype) ? "" : "not ")
+                + "assignable from " + subtype.getName());
+        } catch (BuildException exception) {
+            throw exception;
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+
+            throw exception;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+
+            throw new BuildException(exception);
+        }
+    }
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
