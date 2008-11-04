dd4b591c03de9a6e5426084a9dc3017e9cdba62a
Allen D. Ball <ball@hcf.dev>
2008-11-04 04:27:01 +0000

    Added <list-resources/> build target and Ant Task.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@51 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/ant/taskdefs/ListResourcesTask.java
@@ -0,0 +1,62 @@
+/*
+ * $Id: ListResourcesTask.java,v 1.1 2008-11-04 04:27:01 ball Exp $
+ *
+ * Copyright 2008 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util.ant.taskdefs;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Ant Task to list the resources that match a specific name.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public class ListResourcesTask extends AbstractClasspathTask {
+    private String name = null;
+
+    /**
+     * Sole constructor.
+     */
+    public ListResourcesTask() { super(); }
+
+    protected String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    @Override
+    public void execute() throws BuildException {
+        super.execute();
+
+        if (getName() == null) {
+            throw new BuildException("`name' attribute must be specified");
+        }
+
+        try {
+            log(getName());
+
+            List<URL> list =
+                Collections.list(getClassLoader().getResources(getName()));
+
+            for (URL url : list) {
+                log(String.valueOf(url));
+            }
+        } catch (BuildException exception) {
+            throw exception;
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+
+            throw exception;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+
+            throw new BuildException(exception);
+        }
+    }
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
