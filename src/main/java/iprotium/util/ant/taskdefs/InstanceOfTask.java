7aab2f37983f88b8261dae233a13d34f81fd8adf
Allen D. Ball <ball@hcf.dev>
2008-11-26 08:22:02 +0000

    Added <instance-of/> build target and Ant Task.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@84 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/ant/taskdefs/InstanceOfTask.java
@@ -0,0 +1,108 @@
+/*
+ * $Id: InstanceOfTask.java,v 1.1 2008-11-26 08:22:02 ball Exp $
+ *
+ * Copyright 2008 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util.ant.taskdefs;
+
+import iprotium.util.Factory;
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Ant Task to get an instance of a specified Class.
+ *
+ * @see Factory
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public class InstanceOfTask extends AbstractClasspathTask {
+    private String type = String.class.getName();
+    private List<Argument> list = new ArrayList<Argument>();
+
+    /**
+     * Sole constructor.
+     */
+    public InstanceOfTask() { super(); }
+
+    protected String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public void addConfiguredArgument(Argument argument) { list.add(argument); }
+    protected List<Argument> getArgumentList() { return list; }
+    public void setArgument(String string) { list.add(new Argument(string)); }
+
+    @Override
+    public void execute() throws BuildException {
+        super.execute();
+
+        try {
+            Class<?> type = getClass(getType());
+
+            log(type.getName());
+
+            List<Class> parameters = new ArrayList<Class>();
+            List<Object> arguments = new ArrayList<Object>();
+
+            for (Argument argument : list) {
+                Factory<Object> factory =
+                    new Factory<Object>(getClass(argument.getType()));
+
+                parameters.add(factory.getType());
+                arguments.add(factory.getInstance(argument.getValue()));
+            }
+
+            log(String.valueOf(parameters));
+            log(String.valueOf(arguments));
+
+            Factory<Object> factory = new Factory<Object>(type);
+            Member member = factory.getFactoryMember(toArray(parameters));
+
+            log(String.valueOf(member));
+            log(String.valueOf(factory.apply(member, toArray(arguments))));
+        } catch (BuildException exception) {
+            throw exception;
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+
+            throw exception;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+
+            throw new BuildException(exception);
+        }
+    }
+
+    private Class[] toArray(Collection<Class> collection) {
+        return collection.toArray(new Class[] { });
+    }
+
+    private Object[] toArray(Collection<Object> collection) {
+        return collection.toArray(new Object[] { });
+    }
+
+    public static class Argument {
+        private String type = String.class.getName();
+        private String value = null;
+
+        public Argument(String value) { this.value = value; }
+
+        public Argument() { this(null); }
+
+        protected String getType() { return type; }
+        public void setType(String type) { this.type = type; }
+
+        protected String getValue() { return value; }
+        public void setValue(String value) { this.value = value; }
+
+        @Override
+        public String toString() { return getValue(); }
+    }
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
