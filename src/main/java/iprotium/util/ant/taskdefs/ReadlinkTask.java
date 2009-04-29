249d01a68a4c24fb3edaf5e839d610896b0a0f4f
Allen D. Ball <ball@hcf.dev>
2009-04-29 04:43:15 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@177 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/ant/taskdefs/ReadlinkTask.java
@@ -0,0 +1,52 @@
+/*
+ * $Id: ReadlinkTask.java,v 1.1 2009-04-29 04:43:15 ball Exp $
+ *
+ * Copyright 2009 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util.ant.taskdefs;
+
+import iprotium.text.MapTable;
+import iprotium.util.jni.POSIX;
+import java.io.File;
+import java.util.LinkedHashMap;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Ant Task to read symbolic link targets.
+ *
+ * @see POSIX.readlink(File)
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public class ReadlinkTask extends AbstractMatchingTask {
+
+    /**
+     * Sole constructor.
+     */
+    public ReadlinkTask() { super(); }
+
+    @Override
+    public void execute() throws BuildException {
+        super.execute();
+
+        LinkedHashMap<File,File> map = new LinkedHashMap<File,File>();
+
+        for (File source : getMatchingFileSet()) {
+            File target = POSIX.readlink(source);
+
+            if (target != null) {
+                map.put(source, target);
+            }
+        }
+
+        if (! map.isEmpty()) {
+            for (String line : new MapTable<File,File>(map, "Link", "Target")) {
+                log(line);
+            }
+        }
+    }
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
