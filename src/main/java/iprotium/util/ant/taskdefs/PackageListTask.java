64cd4dc842f190dee57ae76f5909ceae0ecebc4d
Allen D. Ball <ball@hcf.dev>
2009-09-23 05:23:24 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@259 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/ant/taskdefs/PackageListTask.java
@@ -0,0 +1,79 @@
+/*
+ * $Id: PackageListTask.java,v 1.1 2009-09-23 05:23:24 ball Exp $
+ *
+ * Copyright 2009 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util.ant.taskdefs;
+
+import java.util.Set;
+import java.util.TreeSet;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Ant {@link org.apache.tools.ant.Task} to determine the {@link Package}s
+ * represented by the argument {@link Class} files.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public class PackageListTask extends AbstractClassFileTask {
+    private static final String COMMA = ",";
+
+    private String property = null;
+    private String separator = null;
+
+    /**
+     * Sole constructor.
+     */
+    public PackageListTask() {
+        super();
+
+        setSeparator(COMMA);
+    }
+
+    protected String getProperty() { return property; }
+    public void setProperty(String property) { this.property = property; }
+
+    protected String getSeparator() { return separator; }
+    public void setSeparator(String separator) { this.separator = separator; }
+
+    @Override
+    public void execute() throws BuildException {
+        super.execute();
+
+        Set<String> set = new TreeSet<String>();
+
+        for (Class<?> type : getMatchingClassFileMap().values()) {
+            set.add(type.getPackage().getName());
+        }
+
+        if (! set.isEmpty()) {
+            if (getProperty() != null) {
+                getProject().setProperty(getProperty(), toString(set));
+            } else {
+                for (String name : set) {
+                    log(name);
+                }
+            }
+        }
+    }
+
+    private String toString(Iterable<String> iterable) {
+        String string = "";
+
+        for (String name : iterable) {
+            if (string.length() > 0) {
+                if (getSeparator() != null) {
+                    string += getSeparator();
+                }
+            }
+
+            string += name;
+        }
+
+        return string;
+    }
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
