d78fbc9fe42a55c7d39cfe4116c7aeecc1f90bd6
Allen D. Ball <ball@hcf.dev>
2009-10-25 20:52:43 +0000

    Added support for "file" attribute.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@272 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/util/ant/taskdefs/AbstractMatchingTask.java
+++ src/main/java/iprotium/util/ant/taskdefs/AbstractMatchingTask.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AbstractMatchingTask.java,v 1.4 2009-09-04 17:13:43 ball Exp $
+ * $Id: AbstractMatchingTask.java,v 1.5 2009-10-25 20:52:43 ball Exp $
  *
  * Copyright 2008, 2009 Allen D. Ball.  All rights reserved.
  */
@@ -15,10 +15,11 @@ import org.apache.tools.ant.taskdefs.MatchingTask;
  * Abstract base class for Ant {@link MatchingTask} implementations.
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public abstract class AbstractMatchingTask extends MatchingTask {
     private File basedir = null;
+    private File file = null;
 
     /**
      * Sole constructor.
@@ -28,9 +29,12 @@ public abstract class AbstractMatchingTask extends MatchingTask {
     protected File getBasedir() { return basedir; }
     public void setBasedir(File basedir) { this.basedir = basedir; }
 
+    protected File getFile() { return file; }
+    public void setFile(File file) { this.file = file; }
+
     @Override
     public void execute() throws BuildException {
-        if (getBasedir() == null) {
+        if (getBasedir() == null && getFile() == null) {
             setBasedir(getProject().resolveFile("."));
         }
     }
@@ -43,6 +47,12 @@ public abstract class AbstractMatchingTask extends MatchingTask {
             for (String path : getDirectoryScanner(base).getIncludedFiles()) {
                 set.add(new File(base, path));
             }
+        }
+
+        File file = getFile();
+
+        if (file != null) {
+            set.add(file);
         }
 
         return set;
