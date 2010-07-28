b0c9dd7fff38cdccec1ff4f253c465daf42c5232
Allen D. Ball <ball@hcf.dev>
2010-07-28 05:27:04 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@342 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/PrimitiveClassMap.java
@@ -0,0 +1,45 @@
+/*
+ * $Id: PrimitiveClassMap.java,v 1.1 2010-07-28 05:27:04 ball Exp $
+ *
+ * Copyright 2010 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * {@link SortedMap} implementation populated with Java primitive
+ * {@link Class}es mapped to their respective wrapper {@link Class}es.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public class PrimitiveClassMap extends TreeMap<Class<?>,Class<?>> {
+    private static final long serialVersionUID = -9112243509682936487L;
+
+    /**
+     * An unmodifiable instance of {@link PrimitiveClassMap}.
+     */
+    public static final SortedMap<Class<?>,Class<?>> INSTANCE =
+        Collections.unmodifiableSortedMap(new PrimitiveClassMap());
+
+    private PrimitiveClassMap() {
+        super(ClassOrder.NAME);
+
+        put(Boolean.TYPE, Boolean.class);
+        put(Byte.TYPE, Byte.class);
+        put(Character.TYPE, Character.class);
+        put(Double.TYPE, Double.class);
+        put(Float.TYPE, Float.class);
+        put(Integer.TYPE, Integer.class);
+        put(Long.TYPE, Long.class);
+        put(Short.TYPE, Short.class);
+        put(Void.TYPE, Void.class);
+    }
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
