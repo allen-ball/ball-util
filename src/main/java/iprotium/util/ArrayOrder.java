f5bff961e62b3dff86ee6f10845acc4d9e3239d0
Allen D. Ball <ball@hcf.dev>
2010-08-23 03:39:24 +0000

    Element [n] is more significant than element [n - 1].
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@359 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/util/ArrayOrder.java
+++ src/main/java/iprotium/util/ArrayOrder.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ArrayOrder.java,v 1.4 2010-07-28 04:56:50 ball Exp $
+ * $Id: ArrayOrder.java,v 1.5 2010-08-23 03:39:24 ball Exp $
  *
  * Copyright 2008 - 2010 Allen D. Ball.  All rights reserved.
  */
@@ -10,8 +10,10 @@ import java.util.Comparator;
 /**
  * {@link Order} implementation to order arrays.
  *
+ * @param       <T>             The array element type to be compared.
+ *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class ArrayOrder<T> extends Order<T[]> {
     private static final long serialVersionUID = 69419214559265223L;
@@ -39,13 +41,17 @@ public class ArrayOrder<T> extends Order<T[]> {
 
     @Override
     public int compare(T[] left, T[] right) {
-        int comparison = right.length - left.length;
+        int difference = left.length - right.length;
 
-        for (int i = 0, n = left.length; comparison == 0 && i < n; i += 1) {
-            comparison = comparator.compare(left[i], right[i]);
+        for (int i = left.length - 1; i >= 0; i -= 1) {
+            difference = comparator.compare(left[i], right[i]);
+
+            if (difference != 0) {
+                break;
+            }
         }
 
-        return comparison;
+        return difference;
     }
 }
 /*
