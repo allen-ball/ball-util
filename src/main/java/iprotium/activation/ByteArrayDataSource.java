6bff6fdd11ab13c063d1424a6a52156ad6eeebcb
Allen D. Ball <ball@hcf.dev>
2010-09-08 06:41:08 +0000

    Added size() method.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@373 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/activation/ByteArrayDataSource.java
+++ src/main/java/iprotium/activation/ByteArrayDataSource.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ByteArrayDataSource.java,v 1.5 2010-07-28 04:43:42 ball Exp $
+ * $Id: ByteArrayDataSource.java,v 1.6 2010-09-08 06:41:08 ball Exp $
  *
  * Copyright 2009, 2010 Allen D. Ball.  All rights reserved.
  */
@@ -8,22 +8,22 @@ package iprotium.activation;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 
 /**
  * {@link javax.activation.DataSource} implementation based on
  * {@link ByteArrayInputStream} and {@link ByteArrayInputStream}.
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class ByteArrayDataSource extends AbstractDataSource {
-    private ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private ByteArrayOutputStream out = null;
 
     /**
      * @see AbstractDataSource#AbstractDataSource()
      *
      * @param   name            Initial "Name" attribute value.
+     * @param   type            Initial "ContentType" attribute value.
      */
     public ByteArrayDataSource(String name, String type) {
         super();
@@ -33,7 +33,9 @@ public class ByteArrayDataSource extends AbstractDataSource {
         if (type != null) {
             setContentType(type);
         }
+    }
 
+    {
         try {
             getOutputStream().close();
         } catch (IOException exception) {
@@ -47,11 +49,14 @@ public class ByteArrayDataSource extends AbstractDataSource {
     public void reset() { out.reset(); }
 
     /**
+     * See {@link ByteArrayOutputStream#size()}.
+     */
+    public int size() { return out.size(); }
+
+    /**
      * See {@link ByteArrayOutputStream#toByteArray()}.
      */
-    public byte[] toByteArray() {
-        return (out != null) ? out.toByteArray() : new byte[] { };
-    }
+    public byte[] toByteArray() { return out.toByteArray(); }
 
     @Override
     public ByteArrayInputStream getInputStream() throws IOException {
@@ -68,40 +73,6 @@ public class ByteArrayDataSource extends AbstractDataSource {
         }
 
         return out;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-
-        if (getName() != null) {
-            hash ^= getName().hashCode();
-        }
-
-        if (getContentType() != null) {
-            hash ^= getContentType().hashCode();
-        }
-
-        hash ^= ByteBuffer.wrap(toByteArray()).hashCode();
-
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        return (object instanceof ByteArrayDataSource
-                && equals((ByteArrayDataSource) object));
-    }
-
-    private boolean equals(ByteArrayDataSource that) {
-        return (this == that
-                || (equals(this.getName(), that.getName())
-                    && equals(this.getContentType(), that.getContentType())
-                    && (this.hashCode() == that.hashCode())));
-    }
-
-    private <T> boolean equals(T left, T right) {
-        return (left != null) ? left.equals(right) : (left == right);
     }
 }
 /*
