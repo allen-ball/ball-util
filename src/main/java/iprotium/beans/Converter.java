83dcd236f19afdd81567eddfad1f631dee5dd2b3
Allen D. Ball <ball@hcf.dev>
2010-11-04 02:25:43 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@453 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/beans/Converter.java
@@ -0,0 +1,61 @@
+/*
+ * $Id: Converter.java,v 1.1 2010-11-04 02:25:43 ball Exp $
+ *
+ * Copyright 2010 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.beans;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Type {@link Converter} interface.
+ *
+ * @param       <T>             The type of {@link Object} this
+ *                              {@link Converter} will target.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision: 1.1 $
+ */
+public interface Converter<T> {
+
+    /**
+     * Method to convert the argument {@link Object} to {@code this}
+     * {@link Converter}'s type.
+     *
+     * @param   in              The {@link Object} to convert.
+     *
+     * @return  The converted {@link Object}.
+     *
+     * @throws  IllegalAccessException
+     *                          If an underlying
+     *                          {@link java.lang.reflect.Constructor} or
+     *                          {@link java.lang.reflect.Method} enforces
+     *                          Java language access control and the
+     *                          underlying
+     *                          {@link java.lang.reflect.Constructor} or
+     *                          {@link java.lang.reflect.Method} is
+     *                          inaccessible.
+     * @throws  InstantiationException
+     *                          If an underlying
+     *                          {@link java.lang.reflect.Constructor} or
+     *                          {@link java.lang.reflect.Method} represents
+     *                          an abstract {@link Class}.
+     * @throws  InvocationTargetException
+     *                          If an underlying
+     *                          {@link java.lang.reflect.Constructor} or
+     *                          {@link java.lang.reflect.Method} fails for
+     *                          some reason.
+     * @throws  NoSuchMethodException
+     *                          If there is no
+     *                          {@link java.lang.reflect.Constructor} or
+     *                          {@link java.lang.reflect.Method} to
+     *                          facilitate the conversion.
+     */
+    public T convert(Object in) throws IllegalAccessException,
+                                       InstantiationException,
+                                       InvocationTargetException,
+                                       NoSuchMethodException;
+}
+/*
+ * $Log: not supported by cvs2svn $
+ */
