c991770b6a0cadbeda2c2156fa86900307778d09
Allen D. Ball <ball@hcf.dev>
2010-11-08 02:37:37 +0000

    Override convert(Object) to use static decode(String) method.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@470 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/util/IntegerFactory.java
+++ src/main/java/iprotium/util/IntegerFactory.java
@@ -1,15 +1,17 @@
 /*
- * $Id: IntegerFactory.java,v 1.1 2010-11-04 02:40:56 ball Exp $
+ * $Id: IntegerFactory.java,v 1.2 2010-11-08 02:37:37 ball Exp $
  *
  * Copyright 2010 Allen D. Ball.  All rights reserved.
  */
 package iprotium.util;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * {@link Integer} {@link Factory} implementation.
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class IntegerFactory extends Factory<Integer> {
     private static final IntegerFactory DEFAULT = new IntegerFactory();
@@ -25,6 +27,20 @@ public class IntegerFactory extends Factory<Integer> {
      * @see #getDefault()
      */
     protected IntegerFactory() { super(Integer.class); }
+
+    /**
+     * {@inheritDoc}
+     * @see Integer#decode(String)
+     */
+    @Override
+    public Integer convert(Object in) throws IllegalAccessException,
+                                             InstantiationException,
+                                             InvocationTargetException,
+                                             NoSuchMethodException {
+        return ((in instanceof CharSequence)
+                    ? Integer.decode(in.toString())
+                    : super.convert(in));
+    }
 }
 /*
  * $Log: not supported by cvs2svn $
--- src/main/java/iprotium/util/LongFactory.java
+++ src/main/java/iprotium/util/LongFactory.java
@@ -1,15 +1,16 @@
 /*
- * $Id: LongFactory.java,v 1.1 2010-11-04 02:40:56 ball Exp $
+ * $Id: LongFactory.java,v 1.2 2010-11-08 02:37:37 ball Exp $
  *
  * Copyright 2010 Allen D. Ball.  All rights reserved.
  */
 package iprotium.util;
 
+import java.lang.reflect.InvocationTargetException;
 /**
  * {@link Long} {@link Factory} implementation.
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class LongFactory extends Factory<Long> {
     private static final LongFactory DEFAULT = new LongFactory();
@@ -25,6 +26,20 @@ public class LongFactory extends Factory<Long> {
      * @see #getDefault()
      */
     protected LongFactory() { super(Long.class); }
+
+    /**
+     * {@inheritDoc}
+     * @see Long#decode(String)
+     */
+    @Override
+    public Long convert(Object in) throws IllegalAccessException,
+                                          InstantiationException,
+                                          InvocationTargetException,
+                                          NoSuchMethodException {
+        return ((in instanceof CharSequence)
+                    ? Long.decode(in.toString())
+                    : super.convert(in));
+    }
 }
 /*
  * $Log: not supported by cvs2svn $
