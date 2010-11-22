f515f27607b7a40a2d9fb69ab01e3c1138668af2
Allen D. Ball <ball@hcf.dev>
2010-11-22 01:52:24 +0000

    Implement Iterable<E>.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@506 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/util/AbstractIterator.java
+++ src/main/java/iprotium/util/AbstractIterator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AbstractIterator.java,v 1.2 2010-11-09 05:46:28 ball Exp $
+ * $Id: AbstractIterator.java,v 1.3 2010-11-22 01:52:24 ball Exp $
  *
  * Copyright 2010 Allen D. Ball.  All rights reserved.
  */
@@ -16,9 +16,10 @@ import java.util.Iterator;
  *                              {@link Iterator} produces.
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public abstract class AbstractIterator<E> implements Iterator<E>,
+                                                     Iterable<E>,
                                                      Enumeration<E> {
 
     /**
@@ -35,12 +36,27 @@ public abstract class AbstractIterator<E> implements Iterator<E>,
     @Override
     public void remove() { throw new UnsupportedOperationException(); }
 
+    /**
+     * @return  {@code this} {@link Iterator}
+     */
+    @Override
+    public final Iterator<E> iterator() { return this; }
+
+    /**
+     * @see #hasNext()
+     */
     @Override
     public final boolean hasMoreElements() { return hasNext(); }
 
+    /**
+     * @see #next()
+     */
     @Override
     public final E nextElement() { return next(); }
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2010/11/09 05:46:28  ball
+ * Implement Enumeration.
+ *
  */
