3edd0129e622628d65cee7e935b458f647f32a14
Allen D. Ball <ball@hcf.dev>
2010-12-27 02:44:47 +0000

    Support <define/> to specify CPP defines.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/trunk/iprotium.util@574 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/util/ant/taskdefs/JNICCTask.java
+++ src/main/java/iprotium/util/ant/taskdefs/JNICCTask.java
@@ -1,26 +1,25 @@
 /*
- * $Id: JNICCTask.java,v 1.6 2010-08-23 03:43:55 ball Exp $
+ * $Id: JNICCTask.java,v 1.7 2010-12-27 02:44:43 ball Exp $
  *
  * Copyright 2008 - 2010 Allen D. Ball.  All rights reserved.
  */
 package iprotium.util.ant.taskdefs;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.types.DirSet;
 
 /**
  * <a href="http://ant.apache.org/">Ant</a>
  * {@link org.apache.tools.ant.Task} to compile JNI shared objects.
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public class JNICCTask extends AbstractJNIExecuteOnTask {
     private File include = null;
-    private List<DirSet> list = new ArrayList<DirSet>();
+    private TreeMap<String,String> defineMap = new TreeMap<String,String>();
 
     /**
      * Sole constructor.
@@ -34,9 +33,26 @@ public class JNICCTask extends AbstractJNIExecuteOnTask {
     protected File getInclude() { return include; }
     public void setInclude(File include) { this.include = include; }
 
+    public void addConfiguredDefine(Define define) {
+        defineMap.put(define.getName(), define.getValue());
+    }
+
     @Override
     protected String command() {
         String string = getBundleString("cc");
+
+        for (Map.Entry<String,String> entry : defineMap.entrySet()) {
+            string += SPACE;
+            string += getBundleString("cc-D");
+            string += entry.getKey();
+
+            String value = entry.getValue();
+
+            if (value != null) {
+                string += EQUALS;
+                string += value;
+            }
+        }
 
         if (getInclude() != null) {
             string += SPACE;
@@ -48,6 +64,34 @@ public class JNICCTask extends AbstractJNIExecuteOnTask {
         string += getBundleString("cc-args");
 
         return string;
+    }
+
+    /**
+     * {@link JNICCTask} CPP definition.
+     */
+    public static class Define {
+        private String name = null;
+        private String value = null;
+
+        /**
+         * Sole constructor.
+         *
+         * @param       name            The define name.
+         *
+         * @see #setName(String)
+         */
+        public Define(String name) { setName(name); }
+
+        /**
+         * No-argument constructor.
+         */
+        public Define() { this(null); }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+
+        public String getValue() { return value; }
+        public void setValue(String value) { this.value = value; }
     }
 }
 /*
