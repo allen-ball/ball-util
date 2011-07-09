9717a528fc5b185e93d4c90e069d237c6fc68aea
Allen D. Ball <ball@hcf.dev>
2011-07-09 22:33:12 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@693 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/tools/CannotResolveLocationRemedy.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ *
+ * Copyright 2011 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.tools;
+
+import iprotium.util.Regex;
+import java.util.Locale;
+import java.util.SortedSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+
+import static iprotium.lang.java.Keyword.IMPORT;
+import static iprotium.lang.java.Punctuation.SEMICOLON;
+
+/**
+ * {@value CODE} {@link Remedy}.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision$
+ */
+public class CannotResolveLocationRemedy extends Remedy {
+    protected static final String CODE = "compiler.err.cant.resolve.location";
+
+    @Regex
+    private final String REGEX =
+        "(?m)^symbol[\\p{Space}]*:[\\p{Space}]*class[\\p{Space}]+([\\p{Graph}]+)$";
+    private final Pattern PATTERN = Pattern.compile(REGEX);
+
+    private static final String SPACE = " ";
+
+    /**
+     * Sole constructor.
+     */
+    public CannotResolveLocationRemedy() { super(); }
+
+    @Override
+    public String getCode() { return CODE; }
+
+    @Override
+    public String getRx(Diagnostic<? extends JavaFileObject> diagnostic,
+                        StandardJavaFileManager fm,
+                        SortedSet<Class<?>> classes) {
+        JavaFileObject source = diagnostic.getSource();
+        String message = diagnostic.getMessage(Locale.ROOT);
+        Matcher matcher = PATTERN.matcher(message);
+        Class<?> type = null;
+
+        if (matcher.find()) {
+            String symbol = matcher.group(1);
+
+            for (Class<?> member : classes) {
+                if (symbol.equals(member.getSimpleName())) {
+                    type = member;
+                    break;
+                }
+            }
+        }
+
+        return getRX(type);
+    }
+
+    protected String getRX(Class<?> type) {
+        String remedy = null;
+
+        if (type != null) {
+            remedy =
+                IMPORT.lexeme() + SPACE + type.getName() + SEMICOLON.lexeme();
+        }
+
+        return remedy;
+    }
+}
