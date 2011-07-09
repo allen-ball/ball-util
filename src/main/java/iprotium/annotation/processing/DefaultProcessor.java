cbb4cd43d72892976b083fc9710831484a30b3d8
Allen D. Ball <ball@hcf.dev>
2011-07-09 04:23:22 +0000

    Renamed ConvertWithProcessor to DefaultProcessor and claim annotations that would otherwise cause "warning: No processor claimed any of these annotations" messages.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@692 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/annotation/processing/DefaultProcessor.java
+++ src/main/java/iprotium/annotation/processing/DefaultProcessor.java
@@ -6,26 +6,28 @@
 package iprotium.annotation.processing;
 
 import iprotium.beans.ConvertWith;
-import java.util.Collections;
 import java.util.Set;
+import java.util.TreeSet;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
 /**
- * {@link javax.annotation.processing.Processor} implementation to claim
- * {@link ConvertWith} annotations.
+ * Default {@link javax.annotation.processing.Processor} implementation to
+ * claim annotations.
  *
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
  * @version $Revision$
  */
-public class ConvertWithProcessor extends AbstractProcessor {
+@SupportedAnnotationTypes({ "java.beans.ConstructorProperties" })
+public class DefaultProcessor extends AbstractProcessor {
 
     /**
      * Sole constructor.
      */
-    public ConvertWithProcessor() { super(); }
+    public DefaultProcessor() { super(); }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
@@ -34,7 +36,12 @@ public class ConvertWithProcessor extends AbstractProcessor {
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        return Collections.singleton(ConvertWith.class.getCanonicalName());
+        TreeSet<String> set =
+            new TreeSet<String>(super.getSupportedAnnotationTypes());
+
+        set.add(ConvertWith.class.getCanonicalName());
+
+        return set;
     }
 
     @Override
