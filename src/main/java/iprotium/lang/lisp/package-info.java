33c26b54fe468a1d8dba18577ba777f150025f1c
Allen D. Ball <ball@hcf.dev>
2012-01-01 17:30:14 +0000

    Added iprotium.lang.lisp and iprotium.lang.lisp.scheme packages.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@711 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/lang/lisp/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Lisp language support classes.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ */
+package iprotium.lang.lisp;
---
+++ src/main/java/iprotium/lang/lisp/scheme/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Scheme language support classes.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ */
+package iprotium.lang.lisp.scheme;
