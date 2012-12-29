9fba09133c6123e5c7ba2fca567637512e3eb4d4
Allen D. Ball <ball@hcf.dev>
2012-12-29 01:23:16 +0000

    Re-factored as subclass of AbstractAnnotationProcessor. Verify constant value is a String.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@838 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/annotation/processing/RegexProcessor.java
+++ src/main/java/iprotium/annotation/processing/RegexProcessor.java
@@ -1,23 +1,15 @@
 /*
  * $Id$
  *
- * Copyright 2011 Allen D. Ball.  All rights reserved.
+ * Copyright 2011, 2012 Allen D. Ball.  All rights reserved.
  */
 package iprotium.annotation.processing;
 
 import iprotium.util.Regex;
-import java.util.Collections;
-import java.util.Set;
 import java.util.regex.Pattern;
-import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
-import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.util.ElementFilter;
-
-import static javax.tools.Diagnostic.Kind.ERROR;
 
 /**
  * {@link javax.annotation.processing.Processor} implementation to verify
@@ -27,40 +19,25 @@ import static javax.tools.Diagnostic.Kind.ERROR;
  * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
  * @version $Revision$
  */
-public class RegexProcessor extends AbstractProcessor {
+public class RegexProcessor extends AbstractAnnotationProcessor {
 
     /**
      * Sole constructor.
      */
-    public RegexProcessor() { super(); }
+    public RegexProcessor() { super(Regex.class); }
 
     @Override
-    public SourceVersion getSupportedSourceVersion() {
-        return SourceVersion.latestSupported();
-    }
+    public void process(RoundEnvironment roundEnv,
+                        Element element) throws Exception {
+        Object regex = ((VariableElement) element).getConstantValue();
 
-    @Override
-    public Set<String> getSupportedAnnotationTypes() {
-        return Collections.singleton(Regex.class.getCanonicalName());
-    }
-
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations,
-                           RoundEnvironment roundEnv) {
-        for (Element element :
-                 roundEnv.getElementsAnnotatedWith(Regex.class)) {
-            try {
-                Object regex = ((VariableElement) element).getConstantValue();
-
-                if (regex != null) {
-                    Pattern.compile((String) regex);
-                }
-            } catch (Exception exception) {
-                processingEnv.getMessager()
-                    .printMessage(ERROR, exception.getMessage(), element);
+        if (regex != null) {
+            if (regex instanceof String) {
+                Pattern.compile((String) regex);
+            } else {
+                error(element,
+                      "Constant value is not " + String.class.getSimpleName());
             }
         }
-
-        return true;
     }
 }
