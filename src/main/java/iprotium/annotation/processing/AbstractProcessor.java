c0356039893c5e56aabc74117f6176390aa4b26f
Allen D. Ball <ball@hcf.dev>
2012-12-31 20:35:28 +0000

    Added protected specifiedBy(ExecutableElement) and overrides(ExecutableElement) methods.  Changed error() and warning() methods to accept CharSequence message argument.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@850 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/annotation/processing/AbstractProcessor.java
+++ src/main/java/iprotium/annotation/processing/AbstractProcessor.java
@@ -5,15 +5,23 @@
  */
 package iprotium.annotation.processing;
 
+import java.util.ArrayList;
 import java.util.Set;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.disjoint;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.STATIC;
+import static javax.lang.model.util.ElementFilter.methodsIn;
 import static javax.tools.Diagnostic.Kind.ERROR;
 import static javax.tools.Diagnostic.Kind.WARNING;
 
@@ -64,9 +72,9 @@ public abstract class AbstractProcessor
      * Method to print an error.
      *
      * @param   element         The offending {@link Element}.
-     * @param   message         The message {@link String}.
+     * @param   message         The message {@link CharSequence}.
      */
-    protected void error(Element element, String message) {
+    protected void error(Element element, CharSequence message) {
         processingEnv.getMessager().printMessage(ERROR, message, element);
     }
 
@@ -74,9 +82,115 @@ public abstract class AbstractProcessor
      * Method to print a warning.
      *
      * @param   element         The offending {@link Element}.
-     * @param   message         The message {@link String}.
+     * @param   message         The message {@link CharSequence}.
      */
-    protected void warning(Element element, String message) {
+    protected void warning(Element element, CharSequence message) {
         processingEnv.getMessager().printMessage(WARNING, message, element);
+    }
+
+    /**
+     * Method to return the {@link ExecutableElement}
+     * ({@link java.lang.reflect.Method}) the argument
+     * {@link ExecutableElement} is specified by (if any).
+     *
+     * @param   method          The {@link ExecutableElement}.
+     *
+     * @return  The specification {@link ExecutableElement} if any;
+     *          {@code null} otherwise.
+     *
+     * @see #overrides(ExecutableElement)
+     */
+    protected ExecutableElement specifiedBy(ExecutableElement method) {
+        ExecutableElement specification = overrides(method);
+
+        if (specification != null) {
+            for (;;) {
+                ExecutableElement overridden = overrides(specification);
+
+                if (overridden != null) {
+                    specification = overridden;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return specification;
+    }
+
+    /**
+     * Method to return the {@link ExecutableElement}
+     * ({@link java.lang.reflect.Method}) the argument
+     * {@link ExecutableElement} overrides (if any).
+     *
+     * @param   overrider       The {@link ExecutableElement}.
+     *
+     * @return  The overridden {@link ExecutableElement} if any;
+     *          {@code null} otherwise.
+     *
+     * @see Elements#overrides(ExecutableElement,ExecutableElement,TypeElement)
+     */
+    protected ExecutableElement overrides(ExecutableElement overrider) {
+        ExecutableElement overridden = null;
+        TypeElement type = (TypeElement) overrider.getEnclosingElement();
+        ArrayList<TypeMirror> superclasses = new ArrayList<TypeMirror>();
+
+        superclasses.add(type.getSuperclass());
+        superclasses.addAll(type.getInterfaces());
+
+        for (TypeMirror superclass : superclasses) {
+            overridden = overrides(overrider, types.asElement(superclass));
+
+            if (overridden != null) {
+                break;
+            }
+        }
+
+        return overridden;
+    }
+
+    private ExecutableElement overrides(ExecutableElement overrider,
+                                        Element type) {
+        ExecutableElement overridden = null;
+
+        if (overridden == null) {
+            if (type != null) {
+                switch (type.getKind()) {
+                case CLASS:
+                case INTERFACE:
+                    overridden = overridden(overrider, (TypeElement) type);
+                    break;
+
+                default:
+                    break;
+                }
+            }
+        }
+
+        return overridden;
+    }
+
+    private ExecutableElement overridden(ExecutableElement overrider,
+                                         TypeElement type) {
+        ExecutableElement overridden = null;
+
+        if (overridden == null) {
+            for (ExecutableElement method :
+                     methodsIn(type.getEnclosedElements())) {
+                if (disjoint(method.getModifiers(), asList(PRIVATE, STATIC))) {
+                    if (elements.overrides(overrider, method, type)) {
+                        overridden = method;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (overridden == null) {
+            overridden =
+                overrides(overrider, types.asElement(type.getSuperclass()));
+        }
+
+        return overridden;
     }
 }
