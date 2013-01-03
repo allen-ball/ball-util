596fff568a4be8bed8516d798cf3e1af703e9849
Allen D. Ball <ball@hcf.dev>
2013-01-03 17:30:41 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@873 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/annotation/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides {@link javax.annotation.Annotation} implementation classes.
+ *
+ * @see javax.annotation
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ */
+package iprotium.annotation;
