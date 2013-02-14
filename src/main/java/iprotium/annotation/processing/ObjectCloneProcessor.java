64f929d33d60d12ccc619263d08f9768146a7c71
Allen D. Ball <ball@hcf.dev>
2013-02-14 03:59:27 +0000

    Added a check that the clone() implementation should throw a subtype of the implementation type.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@912 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/annotation/processing/ObjectCloneProcessor.java
+++ src/main/java/iprotium/annotation/processing/ObjectCloneProcessor.java
@@ -5,11 +5,13 @@
  */
 package iprotium.annotation.processing;
 
+import java.util.ArrayList;
 import java.util.Set;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
 
 import static javax.lang.model.util.ElementFilter.methodsIn;
 
@@ -21,7 +23,11 @@ import static javax.lang.model.util.ElementFilter.methodsIn;
  *     The implementing {@link Class} also implements {@link Cloneable}
  *   </li>
  *   <li value="2">
- *     The implementation throws {@link CloneNotSupportedException}
+ *     The implementation throws {@link CloneNotSupportedException} (unless
+ *     some "intravening" superclass' implementation does not)
+ *   </li>
+ *   <li value="3">
+ *     The implementation returns a subtype of the implementation type
  *   </li>
  * </ol>
  *
@@ -29,10 +35,9 @@ import static javax.lang.model.util.ElementFilter.methodsIn;
  * @version $Revision$
  */
 public class ObjectCloneProcessor extends AbstractNoAnnotationProcessor {
-    private TypeElement TYPE = null;
-    private ExecutableElement METHOD = null;
-    private TypeElement INTERFACE = null;
-    private TypeElement THROWABLE = null;
+    private TypeElement OBJECT = null;
+    private TypeElement CLONEABLE = null;
+    private ExecutableElement CLONE = null;
 
     /**
      * Sole constructor.
@@ -42,20 +47,20 @@ public class ObjectCloneProcessor extends AbstractNoAnnotationProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations,
                            RoundEnvironment roundEnv) {
-        if (TYPE == null) {
-            TYPE = getTypeElement(Object.class);
+        synchronized (this) {
+            if (OBJECT == null) {
+                OBJECT = getTypeElement(Object.class);
+                CLONEABLE = getTypeElement(Cloneable.class);
 
-            for (ExecutableElement method :
-                     methodsIn(TYPE.getEnclosedElements())) {
-                if (method.getSimpleName().contentEquals("clone")
-                    && method.getParameters().isEmpty()) {
-                    METHOD = method;
-                    break;
+                for (ExecutableElement method :
+                         methodsIn(OBJECT.getEnclosedElements())) {
+                    if (method.getSimpleName().contentEquals("clone")
+                        && method.getParameters().isEmpty()) {
+                        CLONE = method;
+                        break;
+                    }
                 }
             }
-
-            INTERFACE = getTypeElement(Cloneable.class);
-            THROWABLE = getTypeElement(CloneNotSupportedException.class);
         }
 
         return super.process(annotations, roundEnv);
@@ -64,25 +69,55 @@ public class ObjectCloneProcessor extends AbstractNoAnnotationProcessor {
     @Override
     protected void process(Iterable<? extends Element> iterable) {
         for (ExecutableElement method : methodsIn(iterable)) {
-            if (overrides(method, METHOD)) {
-                TypeElement type = (TypeElement) method.getEnclosingElement();
-
-                if (! type.getInterfaces().contains(INTERFACE.asType())) {
-                    warning(type,
-                            type.getKind() + " overrides "
-                            + TYPE.getSimpleName() + "." + METHOD
-                            + " but does not implement "
-                            + INTERFACE.getQualifiedName());
-                }
-
-                if (! method.getThrownTypes().contains(THROWABLE.asType())) {
-                    warning(method,
-                            method.getKind() + " overrides "
-                            + TYPE.getSimpleName() + "." + METHOD
-                            + " but does not throw "
-                            + THROWABLE.getQualifiedName());
-                }
+            if (overrides(method, CLONE)) {
+                check(method);
             }
+        }
+    }
+
+    private void check(ExecutableElement clone) {
+        TypeElement type = (TypeElement) clone.getEnclosingElement();
+
+        if (! type.getInterfaces().contains(CLONEABLE.asType())) {
+            warning(type,
+                    type.getKind() + " overrides "
+                    + OBJECT.getSimpleName() + "." + CLONE
+                    + " but does not implement "
+                    + CLONEABLE.getQualifiedName());
+        }
+
+        if (! types.isAssignable(clone.getReturnType(), type.asType())) {
+            warning(clone,
+                    clone.getKind() + " overrides "
+                    + OBJECT.getSimpleName() + "." + CLONE
+                    + " but does not return a subclass of "
+                    + type.getSimpleName());
+        }
+
+        ArrayList<TypeMirror> throwables = new ArrayList<TypeMirror>();
+
+        throwables.addAll(CLONE.getThrownTypes());
+        throwables.retainAll(overrides(clone).getThrownTypes());
+        throwables.removeAll(clone.getThrownTypes());
+
+        for (TypeMirror mirror : throwables) {
+            Element element = types.asElement(mirror);
+            CharSequence name = null;
+
+            switch (element.getKind()) {
+            case CLASS:
+                name = ((TypeElement) element).getQualifiedName();
+                break;
+
+            default:
+                name = element.getSimpleName();
+                break;
+            }
+
+            warning(clone,
+                    clone.getKind() + " overrides "
+                    + OBJECT.getSimpleName() + "." + CLONE
+                    + " but does not throw " + name);
         }
     }
 }
