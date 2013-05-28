11e4cd270768045a3c6f40c479fb594149855f7b
Allen D. Ball <ball@hcf.dev>
2013-05-28 06:55:49 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@973 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/annotation/processing/ObjectToStringProcessor.java
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.annotation.processing;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+
+import static javax.lang.model.element.ElementKind.CLASS;
+import static javax.lang.model.element.Modifier.ABSTRACT;
+
+/**
+ * {@link javax.annotation.processing.Processor} implementation to check
+ * {@link Class}es to verify:
+ * <ol>
+ *   <li value="1">
+ *     The implementing {@link Class} also overrides {@link Object#toString()}
+ *   </li>
+ * </ol>
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision$
+ */
+@ForElementKinds({ CLASS })
+@ForModifiers(exclude = true, value = { ABSTRACT })
+@ForSubclassesOf(Object.class)
+public class ObjectToStringProcessor extends AbstractNoAnnotationProcessor {
+    private ExecutableElement METHOD = null;
+
+    /**
+     * Sole constructor.
+     */
+    public ObjectToStringProcessor() { super(); }
+
+    @Override
+    public void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+
+        try {
+            METHOD = getExecutableElementFor(Object.class, "toString");
+        } catch (Exception exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+
+    @Override
+    protected void process(Element element) {
+        TypeElement type = (TypeElement) element;
+        ExecutableElement method = implementationOf(METHOD, type);
+
+        if (method == null || METHOD.equals(method)) {
+            warning(type,
+                    type.getKind() + " does not override "
+                    + METHOD.getEnclosingElement().getSimpleName()
+                    + DOT + METHOD.toString());
+        }
+    }
+}
