5e7d7ea8b80f78a91b38d63195a17964b574afaa
Allen D. Ball <ball@hcf.dev>
2013-05-28 06:12:41 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@969 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/annotation/processing/ForModifiers.java
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.annotation.processing;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.lang.model.element.Modifier;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * {@link AbstractNoAnnotationProcessor}
+ * {@link java.lang.annotation.Annotation} to specify {@link Modifier}
+ * criteria.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision$
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE })
+public @interface ForModifiers {
+    boolean exclude() default false;
+    Modifier[] value();
+}
---
+++ src/main/java/iprotium/annotation/processing/ForModifiersProcessor.java
@@ -0,0 +1,54 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.annotation.processing;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+
+/**
+ * {@link javax.annotation.processing.Processor} implementation to check
+ * {@link ForModifiers} annotations.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision$
+ */
+public class ForModifiersProcessor extends AbstractAnnotationProcessor {
+    private TypeElement supertype = null;
+
+    /**
+     * Sole constructor.
+     */
+    public ForModifiersProcessor() { super(ForModifiers.class); }
+
+    @Override
+    public void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+
+        supertype = getTypeElementFor(AbstractNoAnnotationProcessor.class);
+    }
+
+    @Override
+    public void process(RoundEnvironment roundEnv,
+                        Element element) throws Exception {
+        switch (element.getKind()) {
+        case CLASS:
+            if (! isAssignable(element, supertype)) {
+                error(element,
+                      element.getKind() + " annotated with "
+                      + AT + type.getSimpleName()
+                      + " but is not a subclass of "
+                      + supertype.getQualifiedName());
+            }
+            break;
+
+        default:
+            throw new IllegalStateException();
+            /* break; */
+        }
+    }
+}
