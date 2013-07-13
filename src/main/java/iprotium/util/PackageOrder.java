d9bf8c51b525c56e5923e2b4253f4d0b3451b4ce
Allen D. Ball <ball@hcf.dev>
2013-07-13 15:12:37 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@1037 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/util/PackageOrder.java
@@ -0,0 +1,37 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.util;
+
+import iprotium.util.ComparableUtil;
+
+/**
+ * {@link Order} implementation for ordering {@link Package} objects.
+ *
+ * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
+ * @version $Revision$
+ */
+public class PackageOrder extends Order<Package> {
+    private static final long serialVersionUID = 4673155343065186825L;
+
+    /**
+     * Public static instance of {@link PackageOrder}.
+     */
+    public static final PackageOrder INSTANCE = new PackageOrder();
+
+    /**
+     * Sole constructor.
+     */
+    public PackageOrder() { super(); }
+
+    @Override
+    public int compare(Package left, Package right) {
+        return ComparableUtil.compare(getName(left), getName(right));
+    }
+
+    private String getName(Package pkg) {
+        return (pkg != null) ? pkg.getName() : null;
+    }
+}
