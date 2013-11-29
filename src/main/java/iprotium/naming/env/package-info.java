c40cdada1750b8ec47392460e8a67606b817f797
Allen D. Ball <ball@hcf.dev>
2013-11-29 17:15:34 +0000

    Moved JNDI implementation example to example.naming.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@1244 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/naming/env/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Provides {@link iprotium.naming.env.envURLContextFactory}.
- *
- * @author <a href="mailto:ball@iprotium.com">Allen D. Ball</a>
- */
-@ManifestSection
-package iprotium.naming.env;
-
-import iprotium.annotation.ManifestSection;
