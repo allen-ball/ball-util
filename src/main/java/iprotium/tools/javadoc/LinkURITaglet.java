e216c5be83a719bd2b7f39947b4791bb160f9082
Allen D. Ball <ball@hcf.dev>
2013-12-08 01:20:42 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@1266 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/tools/javadoc/LinkURITaglet.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ *
+ * Copyright 2013 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.tools.javadoc;
+
+import com.sun.javadoc.Tag;
+import com.sun.tools.doclets.Taglet;
+import iprotium.annotation.ServiceProviderFor;
+import java.net.URI;
+import java.util.Map;
+
+/**
+ * Inline {@link Taglet} to provide external links.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+@ServiceProviderFor({ Taglet.class })
+@TagletName("link.uri")
+public class LinkURITaglet extends AbstractTaglet {
+    public static void register(Map<String,Taglet> map) {
+        Taglet taglet = new LinkURITaglet();
+        String key = taglet.getName();
+
+        map.remove(key);
+        map.put(key, taglet);
+    }
+
+    /**
+     * Sole constructor.
+     */
+    public LinkURITaglet() { super(true, true, true, true, true, true, true); }
+
+    @Override
+    public String toString(Tag tag) {
+        String in = tag.text().trim();
+        String out = in;
+
+        try {
+            String[] argv = in.split("[\\p{Space}]+", 2);
+            URI href = new URI(argv[0]);
+            String text = (argv.length > 1) ? argv[1] : null;
+
+            out = toString(a(text, href));
+        } catch (Exception exception) {
+        }
+
+        return out;
+    }
+}
