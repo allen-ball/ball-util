0222598e3520e4374be93b035713227cf94d3504
Allen D. Ball <ball@hcf.dev>
2013-12-30 06:21:30 +0000

    Removed unused packages.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@1328 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/iprotium/xml/bind/annotation/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * See {@link javax.xml.bind.annotation}.
- *
- * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
- */
-@ManifestSection
-package iprotium.xml.bind.annotation;
-
-import iprotium.annotation.ManifestSection;
