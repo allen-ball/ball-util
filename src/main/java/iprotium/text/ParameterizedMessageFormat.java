49808d25ec0053bac834b2a22f4dba8fd806cf4f
Allen D. Ball <ball@hcf.dev>
2014-01-02 17:40:04 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/iprotium.util/trunk@1359 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/iprotium/text/ParameterizedMessageFormat.java
@@ -0,0 +1,49 @@
+/*
+ * $Id$
+ *
+ * Copyright 2014 Allen D. Ball.  All rights reserved.
+ */
+package iprotium.text;
+
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * {@link MessageFormat} subclass that provides {@link #format(String,Map)}
+ * method that translates the pattern {@link String} and named to parameters
+ * in the argument {@link Map} to positional parameters for a call to
+ * {@link #format(String,Object...)}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class ParameterizedMessageFormat extends MessageFormat {
+    private ParameterizedMessageFormat() { super(null); }
+
+    /**
+     * Method that translates the pattern {@link String} and named to
+     * parameters in the argument {@link Map} to positional parameters for a
+     * call to {@link #format(String,Object...)}.
+     *
+     * @param   pattern         The pattern {@link String}.
+     * @param   map             The parameter {@link Map}.
+     *
+     * @return  The formatted {@link String}.
+     */
+    public static String format(String pattern, Map<String,?> map) {
+        Object[] values = new Object[map.size()];
+        int i = 0;
+
+        for (Map.Entry<String,?> entry : map.entrySet()) {
+            pattern =
+                pattern.replaceAll(Pattern.quote("{" + entry.getKey() + "}"),
+                                   "{" + i + "}");
+            values[i] = entry.getValue();
+
+            i += 1;
+        }
+
+        return format(pattern, values);
+    }
+}
