0a28953b07b339ac633edfa3e4459b6e3f9222fe
Allen D. Ball <ball@hcf.dev>
2014-08-04 00:16:38 +0000

    Added ball.swing.table package.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@1551 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/swing/table/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * {@link javax.swing.table} implementation classes.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ */
+@ManifestSection
+package ball.swing.table;
+
+import ball.annotation.ManifestSection;
