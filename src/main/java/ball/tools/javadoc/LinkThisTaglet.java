5de47a5614c74aaffe419397a6d96d8d60cabada
Allen D. Ball <ball@hcf.dev>
2015-08-02 05:14:50 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@1737 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/tools/javadoc/LinkThisTaglet.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ *
+ * Copyright 2015 Allen D. Ball.  All rights reserved.
+ */
+package ball.tools.javadoc;
+
+import ball.annotation.ServiceProviderFor;
+import ball.lang.Keyword;
+import ball.xml.HTML;
+import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.Doc;
+import com.sun.javadoc.Tag;
+import com.sun.tools.doclets.internal.toolkit.taglets.Taglet;
+import com.sun.tools.doclets.internal.toolkit.taglets.TagletOutput;
+import com.sun.tools.doclets.internal.toolkit.taglets.TagletWriter;
+import java.util.Map;
+import org.w3c.dom.Element;
+
+import static ball.util.StringUtil.isNil;
+
+/**
+ * Inline {@link Taglet} to provide {@link.this} links.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+@ServiceProviderFor({ Taglet.class })
+@TagletName("link.this")
+public class LinkThisTaglet extends AbstractInlineTaglet {
+    public static void register(Map<String,Taglet> map) {
+        register(LinkThisTaglet.class, map);
+    }
+
+    /**
+     * Sole constructor.
+     */
+    public LinkThisTaglet() { super(); }
+
+    @Override
+    public TagletOutput getTagletOutput(Tag tag,
+                                        TagletWriter writer) throws IllegalArgumentException {
+        setConfiguration(writer.configuration());
+
+        Element element = HTML.code(document, Keyword.THIS.lexeme());
+
+        try {
+            if (! isNil(tag.text().trim())) {
+                throw new IllegalArgumentException("Invalid argument");
+            }
+
+            ClassDoc target = getContainingClassDoc(tag.holder());
+
+            if (target != null) {
+                element =
+                    (Element) getClassDocLink(tag.holder(), element, target);
+            }
+        } catch (Exception exception) {
+            throw new IllegalArgumentException(tag.position().toString(),
+                                               exception);
+        }
+
+        return output(writer, element);
+    }
+}
