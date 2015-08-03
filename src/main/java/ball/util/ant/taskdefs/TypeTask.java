fe695e8304cd8512d8bade4c32513e296d804018
Allen D. Ball <ball@hcf.dev>
2015-08-03 00:04:24 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@1738 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/util/ant/taskdefs/TypeTask.java
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ *
+ * Copyright 2015 Allen D. Ball.  All rights reserved.
+ */
+package ball.util.ant.taskdefs;
+
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Abstract {@link.uri http://ant.apache.org/ Ant}
+ * {@link org.apache.tools.ant.Task} to specify a type ({@link Class}).
+ *
+ * {@bean-info}
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class TypeTask extends AbstractClasspathTask {
+    private String type = null;
+
+    /**
+     * Sole constructor.
+     */
+    protected TypeTask() { super(); }
+
+    @NotNull
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+}
