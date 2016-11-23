20d6c452967435593ce891850ea09ff6e9f2e871
Allen D. Ball <ball@hcf.dev>
2016-11-23 21:12:40 +0000

    Added entrySet() implementation.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@2057 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/util/MapView.java
+++ src/main/java/ball/util/MapView.java
@@ -7,6 +7,8 @@ package ball.util;
 
 import java.io.Serializable;
 import java.util.AbstractMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -23,9 +25,10 @@ import java.util.Set;
  * @version $Revision$
  */
 public class MapView<K,V> extends AbstractMap<K,V> implements Serializable {
-    private static final long serialVersionUID = 5823793890947160939L;
+    private static final long serialVersionUID = -1914866590078121842L;
 
     private final Map<K,V> map;
+    protected final EntrySet entrySet = new EntrySet();
 
     /**
      * Sole constructor.
@@ -55,6 +58,73 @@ public class MapView<K,V> extends AbstractMap<K,V> implements Serializable {
     @Override
     public void clear() { map().clear(); }
 
+    /**
+     * Method returns a single {@link Set} backed by {@link #map()}.
+     * Subclass implementations should update {@link #entrySet}.
+     *
+     * @return  {@link #entrySet}
+     */
     @Override
-    public Set<Entry<K,V>> entrySet() { return map().entrySet(); }
+    public Set<Entry<K,V>> entrySet() {
+        entrySet.clear();
+        entrySet.addAll(map().entrySet());
+
+        return entrySet;
+    }
+
+    /**
+     * {@link #entrySet} implementation class.
+     */
+    protected class EntrySet extends LinkedHashSet<Entry<K,V>> {
+        private static final long serialVersionUID = 5015923978722180032L;
+
+        /**
+         * Sole constructor.
+         */
+        public EntrySet() { super(); }
+
+        @Override
+        public boolean remove(Object object) {
+            boolean changed = super.remove(object);
+
+            if (changed) {
+                MapView.this.remove(((Entry<?,?>) object).getKey());
+            }
+
+            return changed;
+        }
+
+        @Override
+        public Iterator<Entry<K,V>> iterator() {
+            return new EntryIterator(super.iterator());
+        }
+    }
+
+    private class EntryIterator implements Iterator<Entry<K,V>> {
+        private final Iterator<Entry<K,V>> iterator;
+        private Entry<K,V> current = null;
+
+        public EntryIterator(Iterator<Entry<K,V>> iterator) {
+            this.iterator = iterator;
+        }
+
+        @Override
+        public boolean hasNext() { return iterator.hasNext(); }
+
+        @Override
+        public Entry<K,V> next() {
+            current = iterator.next();
+
+            return current;
+        }
+
+        @Override
+        public void remove() {
+            iterator.remove();
+            MapView.this.remove(current.getKey());
+        }
+
+        @Override
+        public String toString() { return iterator.toString(); }
+    }
 }
