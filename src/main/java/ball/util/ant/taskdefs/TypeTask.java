4546d0075aff2a94f39c9b24d605bc062090ed3b
Allen D. Ball <ball@hcf.dev>
2017-03-05 04:40:27 +0000

    Moved TypeTask implementations to nested inner classes.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@2098 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/util/ant/taskdefs/TypeTask.java
+++ src/main/java/ball/util/ant/taskdefs/TypeTask.java
@@ -1,11 +1,26 @@
 /*
  * $Id$
  *
- * Copyright 2015, 2016 Allen D. Ball.  All rights reserved.
+ * Copyright 2015 - 2017 Allen D. Ball.  All rights reserved.
  */
 package ball.util.ant.taskdefs;
 
+import ball.swing.table.ArrayListTableModel;
+import ball.swing.table.SimpleTableModel;
+import ball.util.BeanInfoUtil;
+import ball.util.SuperclassSet;
+import java.beans.BeanDescriptor;
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.TypeVariable;
+import java.util.Arrays;
 import org.apache.tools.ant.BuildException;
+
+import static ball.lang.Punctuation.GT;
+import static ball.lang.Punctuation.LT;
+import static ball.util.ClassOrder.INHERITANCE;
+import static ball.util.StringUtil.NIL;
 
 /**
  * Abstract {@link.uri http://ant.apache.org/ Ant}
@@ -27,4 +42,208 @@ public abstract class TypeTask extends AbstractClasspathTask {
     @NotNull
     public String getType() { return type; }
     public void setType(String type) { this.type = type; }
+
+    /**
+     * {@link.uri http://ant.apache.org/ Ant} {@link org.apache.tools.ant.Task}
+     * to display {@link BeanInfo} for a specified {@link Class}.
+     *
+     * {@bean.info}
+     */
+    @AntTask("bean-info-for")
+    public class BeanInfoFor extends TypeTask {
+
+        /**
+         * Sole constructor.
+         */
+        public BeanInfoFor() { super(); }
+
+        @Override
+        public void execute() throws BuildException {
+            super.execute();
+
+            try {
+                log(Introspector.getBeanInfo(Class.forName(getType(),
+                                                           false,
+                                                           getClassLoader())));
+            } catch (BuildException exception) {
+                throw exception;
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+                throw new BuildException(throwable);
+            }
+        }
+
+        private void log(BeanInfo bean) {
+            log(new BeanHeaderTableModel(bean.getBeanDescriptor()));
+            log(NIL);
+            log(new BeanPropertyTableModel(bean.getPropertyDescriptors()));
+            log(bean.getAdditionalBeanInfo());
+        }
+
+        private void log(BeanInfo[] beans) {
+            if (beans != null) {
+                for (BeanInfo bean : beans) {
+                    log(NIL);
+                    log(bean);
+                }
+            }
+        }
+
+        private class BeanHeaderTableModel extends SimpleTableModel {
+            private static final long serialVersionUID = 2338583773511453873L;
+
+            public BeanHeaderTableModel(BeanDescriptor descriptor) {
+                super(new Object[][] { }, 2);
+
+                row("Bean Class:",
+                    descriptor.getBeanClass().getName());
+
+                if (descriptor.getCustomizerClass() != null) {
+                    row("Customizer Class:",
+                        descriptor.getCustomizerClass().getName());
+                }
+            }
+        }
+
+        private class BeanPropertyTableModel
+                      extends ArrayListTableModel<PropertyDescriptor> {
+            private static final long serialVersionUID = -5252202218618591258L;
+
+            public BeanPropertyTableModel(PropertyDescriptor[] rows) {
+                super(Arrays.asList(rows),
+                      "Name", "Mode", "Type",
+                      "isHidden", "isBound", "isConstrained");
+            }
+
+            @Override
+            protected Object getValueAt(PropertyDescriptor row, int x) {
+                Object value = null;
+
+                switch (x) {
+                case 0:
+                default:
+                    value = row.getName();
+                    break;
+
+                case 1:
+                    value = BeanInfoUtil.getMode(row);
+                    break;
+
+                case 2:
+                    value = getCanonicalName(row.getPropertyType());
+                    break;
+
+                case 3:
+                    value = row.isHidden();
+                    break;
+
+                case 4:
+                    value = row.isBound();
+                    break;
+
+                case 5:
+                    value = row.isConstrained();
+                    break;
+                }
+
+                return value;
+            }
+
+            private String getCanonicalName(Class<?> type) {
+                return (type != null) ? type.getCanonicalName() : NIL;
+            }
+        }
+    }
+
+    /**
+     * {@link.uri http://ant.apache.org/ Ant} {@link org.apache.tools.ant.Task}
+     * to display superclasses of a specified {@link Class}.
+     *
+     * {@bean.info}
+     */
+    @AntTask("is-assignable-from")
+    public class IsAssignableFrom extends TypeTask {
+        private String subtype = null;
+
+        /**
+         * Sole constructor.
+         */
+        public IsAssignableFrom() { super(); }
+
+        @NotNull
+        public String getSubtype() { return subtype; }
+        public void setSubtype(String subtype) { this.subtype = subtype; }
+
+        @Override
+        public void execute() throws BuildException {
+            super.execute();
+
+            try {
+                Class<?> supertype =
+                    Class.forName(getType(), false, getClassLoader());
+                Class<?> subtype =
+                    Class.forName(getSubtype(), false, supertype.getClassLoader());
+
+                log(supertype.getName() + " is "
+                    + (supertype.isAssignableFrom(subtype) ? "" : "not ")
+                    + "assignable from " + subtype.getName());
+            } catch (BuildException exception) {
+                throw exception;
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+                throw new BuildException(throwable);
+            }
+        }
+    }
+
+    /**
+     * {@link.uri http://ant.apache.org/ Ant} {@link org.apache.tools.ant.Task}
+     * to display superclasses of a specified {@link Class}.
+     *
+     * {@bean.info}
+     */
+    @AntTask("superclasses-of")
+    public class SuperclassesOf extends TypeTask {
+
+        /**
+         * Sole constructor.
+         */
+        public SuperclassesOf() { super(); }
+
+        @Override
+        public void execute() throws BuildException {
+            super.execute();
+
+            try {
+                Class<?> type =
+                    Class.forName(getType(), false, getClassLoader());
+
+                for (Class<?> superclass :
+                         INHERITANCE.asList(new SuperclassSet(type))) {
+                    log(toString(superclass));
+                }
+            } catch (BuildException exception) {
+                throw exception;
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+                throw new BuildException(throwable);
+            }
+        }
+
+        private String toString(Class<?> type) {
+            StringBuilder buffer = new StringBuilder(type.getName());
+
+            if (type.getTypeParameters().length > 0) {
+                buffer.append(LT.lexeme());
+
+                for (TypeVariable<?> parameter : type.getTypeParameters()) {
+                    buffer.append(parameter);
+                }
+
+                buffer.append(GT.lexeme());
+            }
+
+            return buffer.toString();
+        }
+    }
 }
