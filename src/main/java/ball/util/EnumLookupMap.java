45c3025cc713cee90d9ef88ae16e332735b07e41
Allen D. Ball <ball@hcf.dev>
2017-09-14 00:29:54 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@2385 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/util/EnumLookupMap.java
@@ -0,0 +1,36 @@
+/*
+ * $Id$
+ *
+ * Copyright 2017 Allen D. Ball.  All rights reserved.
+ */
+package ball.util;
+
+import java.util.TreeMap;
+
+/**
+ * {@link java.util.SortedMap} implementation which provides
+ * {@link String#CASE_INSENSITIVE_ORDER} look-up by name to the
+ * corresponding {@link Enum}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public class EnumLookupMap extends TreeMap<String,Enum<?>> {
+    private static final long serialVersionUID = -3569976523862652175L;
+
+    /**
+     * Sole constructor.
+     *
+     * @param   types           The {@link Enum} types to include.
+     */
+    @SafeVarargs
+    public EnumLookupMap(Class<? extends Enum<?>>... types) {
+        super(String.CASE_INSENSITIVE_ORDER);
+
+        for (Class<? extends Enum<?>> type : types) {
+            for (Enum<?> constant : type.getEnumConstants()) {
+                put(constant.name(), constant);
+            }
+        }
+    }
+}
