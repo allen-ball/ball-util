0f3d0699ce9f840fa7f26dc5f700390eb4d78021
Allen D. Ball <ball@hcf.dev>
2019-02-16 19:33:09 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@3753 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/lang/reflect/DefaultInvocationHandler.java
@@ -0,0 +1,84 @@
+/*
+ * $Id$
+ *
+ * Copyright 2019 Allen D. Ball.  All rights reserved.
+ */
+package ball.lang.reflect;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import static org.apache.commons.lang3.reflect.MethodUtils.invokeMethod;
+
+/**
+ * Default {@link InvocationHandler} implementation.
+ * See {@link #invoke(Object,Method,Object[])}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public class DefaultInvocationHandler implements InvocationHandler {
+
+    /**
+     * Sole constructor.
+     */
+    public DefaultInvocationHandler() { }
+
+    /**
+     * Method to call
+     * {@link Proxy#newProxyInstance(ClassLoader,Class[],InvocationHandler)}
+     * with {@link.this} {@link InvocationHandler}.
+     *
+     * @param   interfaces      The interface {@link Class}es the
+     *                          {@link Proxy} {@link Class} will implement.
+     *
+     * @return  The {@link Proxy}.
+     */
+    public Object newProxyInstance(Class<?>... interfaces) {
+        return Proxy.newProxyInstance(getClass().getClassLoader(),
+                                      interfaces, this);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * If the {@link Method#isDefault() method.isDefault()}, that
+     * {@link Method} will be invoked directly.  Otherwise, the call will
+     * be dispatched to a declared {@link Method} on {@link.this}
+     * {@link InvocationHandler} with the same name and compatible parameter
+     * types (forcing access if necessary).
+     *
+     * @throws  Exception       If no compatible {@link Method} is found or
+     *                          the {@link Method} cannot be invoked.
+     */
+    @Override
+    public Object invoke(Object proxy,
+                         Method method, Object[] argv) throws Throwable {
+        Object result = null;
+
+        if (method.isDefault()) {
+            Class<?> declarer = method.getDeclaringClass();
+            Constructor<MethodHandles.Lookup> constructor =
+                MethodHandles.Lookup.class
+                .getDeclaredConstructor(Class.class);
+
+            constructor.setAccessible(true);
+
+            result =
+                constructor.newInstance(declarer)
+                .in(declarer)
+                .unreflectSpecial(method, declarer)
+                .bindTo(proxy)
+                .invokeWithArguments(argv);
+        } else {
+            result =
+                invokeMethod(this, true, method.getName(),
+                             argv, method.getParameterTypes());
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/ball/lang/reflect/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ *
+ * Copyright 2019 Allen D. Ball.  All rights reserved.
+ */
+/**
+ * See {@link java.lang.reflect}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ */
+@Manifest.Section
+package ball.lang.reflect;
+
+import ball.annotation.Manifest;
