3d5cb18aba32d8ae08eff05fe63614960cb388ba
Allen D. Ball <ball@hcf.dev>
2019-02-18 01:18:56 +0000

    Interim check-in.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@3763 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/tools/javadoc/MavenAwareStandardDoclet.java
@@ -0,0 +1,93 @@
+/*
+ * $Id$
+ *
+ * Copyright 2019 Allen D. Ball.  All rights reserved.
+ */
+package ball.tools.javadoc;
+
+import com.sun.javadoc.DocErrorReporter;
+import com.sun.javadoc.LanguageVersion;
+import com.sun.javadoc.RootDoc;
+import com.sun.tools.doclets.Taglet;
+import com.sun.tools.doclets.standard.Standard;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ServiceLoader;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * WORK IN PROGRESS: {@link Doclet} implementation to intercept calls to the
+ * {@link Standard} {@link Doclet}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+@NoArgsConstructor @ToString
+public class MavenAwareStandardDoclet extends Standard {
+
+    /**
+     * See {@link Doclet#validOptions(String[][],DocErrorReporter)}.
+     */
+    public static final ThreadLocal<String[][]> OPTIONS = new ThreadLocal<>();
+
+    /**
+     * See {@link Doclet#validOptions(String[][],DocErrorReporter)}.
+     */
+    public static final ThreadLocal<DocErrorReporter> REPORTER =
+        new ThreadLocal<>();
+
+    /**
+     * See {@link Doclet#start(RootDoc)}.
+     */
+    public static final ThreadLocal<RootDoc> ROOT = new ThreadLocal<>();
+
+    /**
+     * See {@link Doclet#validOptions(String[][],DocErrorReporter)}.
+     *
+     * @param   options         The {@code options} and their arguments.
+     * @param   reporter        The {@link DocErrorReporter}.
+     *
+     * @return  {@code true} if the {@code options} are valid; {@code false}
+     *          otherwise.
+     */
+    public static boolean validOptions(String[][] options,
+                                       DocErrorReporter reporter) {
+        Boolean result = null;
+
+        ArrayList<String[]> list = new ArrayList<>(Arrays.asList(options));
+
+        for (Taglet taglet :
+                 ServiceLoader.load(Taglet.class,
+                                    MavenAwareStandardDoclet.class
+                                    .getClassLoader())) {
+            list.add(new String[] { "-taglet", taglet.getClass().getName() });
+        }
+
+        options = list.toArray(new String[][] { });
+
+        OPTIONS.set(options);
+        REPORTER.set(reporter);
+
+        result = Standard.validOptions(options, reporter);
+
+        return (result != null) ? result : false;
+    }
+
+    /**
+     * See {@link Doclet#start(RootDoc)}.
+     *
+     * @param   root            The {@link RootDoc}.
+     *
+     * @return  {@code true} on success; {@code false} otherwise.
+     */
+    public static boolean start(RootDoc root) {
+        Boolean result = null;
+
+        ROOT.set(root);
+
+        result = Standard.start(root);
+
+        return (result != null) ? result : false;
+    }
+}
