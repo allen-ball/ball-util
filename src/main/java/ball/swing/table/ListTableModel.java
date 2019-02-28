1f88627e454cdcb8a985484e910cad2ce36e0edd
Allen D. Ball <ball@hcf.dev>
2019-02-28 16:24:57 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@3808 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/swing/table/ListTableModel.java
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ *
+ * Copyright 2019 Allen D. Ball.  All rights reserved.
+ */
+package ball.swing.table;
+
+import java.util.List;
+import javax.swing.event.TableModelEvent;
+
+/**
+ * {@link List} {@link javax.swing.table.TableModel} implementation.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public class ListTableModel extends ArrayListTableModel<Object> {
+    private static final long serialVersionUID = -3491544585475074062L;
+
+    /** @serial */ private final List<?> list;
+
+    /**
+     * @see AbstractTableModelImpl#AbstractTableModelImpl(String...)
+     *
+     * @param   list            The underlying {@link List}.
+     * @param   name            The column name.
+     */
+    public ListTableModel(List<?> list, String name) {
+        super(list, new String[] { name });
+
+        this.list = list;
+    }
+
+    /**
+     * @see AbstractTableModelImpl#AbstractTableModelImpl(int)
+     *
+     * @param   list             The underlying {@link List}.
+     */
+    public ListTableModel(List<?> list) { this(list, null); }
+
+    @Override
+    protected Object getValueAt(Object row, int x) {
+        Object value = null;
+
+        switch (x) {
+        case 0:
+        default:
+            value = row;
+            break;
+        }
+
+        return value;
+    }
+
+    @Override
+    public void tableChanged(TableModelEvent event) {
+        list().clear();
+        list().addAll(list);
+
+        super.tableChanged(event);
+    }
+}
