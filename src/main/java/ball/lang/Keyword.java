304c822b074ee923654a2dafaf001283ab312d2b
Allen D. Ball <ball@hcf.dev>
2019-10-22 16:14:38 +0000

    * src/main/java/ball/lang/Keyword.java: Added JRE 13 "yield".
    	* src/main/java/ball/lang/Literal.java: Initial writing.
    
    
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@4660 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/lang/Keyword.java
+++ src/main/java/ball/lang/Keyword.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright 2010 - 2014 Allen D. Ball.  All rights reserved.
+ * Copyright 2010 - 2019 Allen D. Ball.  All rights reserved.
  */
 package ball.lang;
 
@@ -16,21 +16,21 @@ import java.util.regex.Pattern;
  */
 public enum Keyword implements Parser.Lexeme {
     ABSTRACT, ASSERT, BOOLEAN, BREAK, BYTE, CASE, CATCH, CHAR, CLASS, CONST,
-        CONTINUE, DEFAULT, DO, DOUBLE, ELSE, ENUM, EXTENDS, FALSE, FINAL,
-        FINALLY, FLOAT, FOR, GOTO, IF, IMPLEMENTS, IMPORT, INSTANCEOF, INT,
-        INTERFACE, LONG, NATIVE, NEW, NULL, PACKAGE, PRIVATE, PROTECTED,
-        PUBLIC, RETURN, SHORT, STATIC, STRICTFP, SUPER, SWITCH, SYNCHRONIZED,
-        THIS, THROW, THROWS, TRANSIENT, TRUE, TRY, VOID, VOLATILE, WHILE;
+        CONTINUE, DEFAULT, DO, DOUBLE, ELSE, ENUM, EXTENDS, FINAL, FINALLY,
+        FLOAT, FOR, GOTO, IF, IMPLEMENTS, IMPORT, INSTANCEOF, INT, INTERFACE,
+        LONG, NATIVE, NEW, PACKAGE, PRIVATE, PROTECTED, PUBLIC, RETURN, SHORT,
+        STATIC, STRICTFP, SUPER, SWITCH, SYNCHRONIZED, THIS, THROW, THROWS,
+        TRANSIENT, TRY, VOID, VOLATILE, WHILE, YIELD;
 
     private final Pattern pattern;
 
     private Keyword() {
-        this.pattern =
-            Pattern.compile("\\b" + Pattern.quote(lexeme()) + "\\B");
+        pattern = Pattern.compile("\\b" + Pattern.quote(lexeme()) + "\\B");
     }
 
     /**
-     * Method to get the lexeme associated with this {@link Keyword}.
+     * Method to get the lexeme associated with this {@link.this}
+     * {@link Keyword}.
      *
      * @return  The lexeme (literal keyword).
      */
