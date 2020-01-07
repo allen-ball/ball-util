e2e18a6a93848491632ba3314d7c0e8952311c07
Allen D. Ball <ball@hcf.dev>
2020-01-07 19:46:03 +0000

    Updated copyright protection notice.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@4981 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/annotation/processing/TagletNameProcessor.java
+++ src/main/java/ball/annotation/processing/TagletNameProcessor.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Copyright 2013 - 2019 Allen D. Ball.  All rights reserved.
+ * Copyright 2013 - 2020 Allen D. Ball.  All rights reserved.
  */
 package ball.annotation.processing;
 
