8c751e29e1a221cb6ae5ecdb0e2c17ebfc7ae452
Allen D. Ball <ball@hcf.dev>
2020-01-16 06:42:04 +0000

    Removed javadoc/overview.html and moved comments to top-level package-info.java.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.util/trunk@5037 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/util/package-info.java
+++ src/main/java/ball/util/package-info.java
@@ -1,10 +1,12 @@
 /*
  * $Id$
  *
- * Copyright 2016 Allen D. Ball.  All rights reserved.
+ * Copyright 2016 - 2020 Allen D. Ball.  All rights reserved.
  */
 /**
  * See {@link java.util}.
+ *
+ * {@pom.coordinates}
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  */
