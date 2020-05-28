fee06e7c6b0be59f4d0cf631a249fec0faac3786
Allen D. Ball <ball@hcf.dev>
2020-05-28 01:20:12 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-util/trunk@6059 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/tools/javac/JavacPluginName.java
@@ -0,0 +1,86 @@
+package ball.tools.javac;
+/*-
+ * ##########################################################################
+ * Utilities
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2008 - 2020 Allen D. Ball
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ##########################################################################
+ */
+import ball.annotation.ServiceProviderFor;
+import ball.annotation.processing.AnnotatedProcessor;
+import ball.annotation.processing.AnnotatedTypeMustExtend;
+import ball.annotation.processing.For;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static javax.lang.model.element.Modifier.ABSTRACT;
+import static javax.tools.Diagnostic.Kind.ERROR;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+/**
+ * {@link java.lang.annotation.Annotation} to mark
+ * {@link com.sun.source.util.Plugin}s with their name.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE })
+@AnnotatedTypeMustExtend(AnnotatedJavacPlugin.class)
+public @interface JavacPluginName {
+    String value();
+
+    /**
+     * {@link Processor} implementation.
+     */
+    @ServiceProviderFor({ Processor.class })
+    @For({ JavacPluginName.class })
+    @NoArgsConstructor @ToString
+    public class ProcessorImpl extends AnnotatedProcessor {
+        @Override
+        protected void process(RoundEnvironment roundEnv,
+                               TypeElement annotation, Element element) {
+            super.process(roundEnv, annotation, element);
+
+            AnnotationMirror mirror = getAnnotationMirror(element, annotation);
+            AnnotationValue value = getAnnotationValue(mirror, "value");
+            String name = (String) value.getValue();
+
+            if (! name.isEmpty()) {
+                if (element.getModifiers().contains(ABSTRACT)) {
+                    print(ERROR, element, mirror,
+                          "%s is %s", element.getKind(), ABSTRACT);
+                }
+            } else {
+                print(ERROR, element, mirror, value,
+                      "value() must be a non-empty String");
+            }
+        }
+    }
+}
