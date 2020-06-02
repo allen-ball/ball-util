ede68cec9808a3f6e4c5b4b72e3d420be8ffc56f
Allen D. Ball <ball@hcf.dev>
2020-06-02 17:11:10 +0000

    Removed @ResourceFile support.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-util/trunk@6093 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/util/ant/taskdefs/AntTask.java
+++ src/main/java/ball/util/ant/taskdefs/AntTask.java
@@ -32,8 +32,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * {@link java.lang.annotation.Annotation} to mark
- * {@link.uri http://ant.apache.org/ Ant}
- * {@link org.apache.tools.ant.Task}s.
+ * {@link.uri http://ant.apache.org/ Ant} {@link Task}s.
  *
  * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
  * @version $Revision$
